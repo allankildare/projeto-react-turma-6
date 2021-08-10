49695351dd1c5837f446f044580d7829d4e612e3
allankildare <allankildare27@gmail.com>
2021-08-10 11:30:29 -0300

    chore: alteracao de estilizacao no App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
-import Button from './components/Button/Button'
-import './App.css';
-import Header from './components/Header/Header';
-import Form from './components/Form/Form';
-import Footer from './components/Footer/Footer';
-import GridExample from './components/GridExample/GridExample';
+import './App.css'
+import Header from './components/Header/Header'
+import Form from './components/Form/Form'
+import Footer from './components/Footer/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <div className="main">
-          <h1>Olá mundo!</h1>
+        <div className="content">
           <Form />
-          <GridExample />
         </div>
         <Footer />
       </div>
