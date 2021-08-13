8ca0837549352f967ee2b9aa74e14f58ac46bb71
allankildare <allankildare27@gmail.com>
2021-08-13 12:18:51 -0300

    feat: componente Button e estados

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,18 +4,31 @@ import style from './Button.module.css'
 class Button extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            disabled: false
+        }
         this.meuClique = this.meuClique.bind(this)
     }
 
     meuClique() {
         console.log('Fui clicado!')
+        this.setState({ disabled: true })
+        
+        setTimeout(() => {
+            this.setState({ disabled: false })
+        }, 3000)
     }
 
     render() {
+        console.log('Renderizei')
         return (
-            <button className={style.button} onClick={this.meuClique}>{this.props.texto}</button>
+            <button disabled={this.state.disabled}
+                className={style.btn}
+                onClick={this.meuClique}>
+                    {this.props.texto}
+            </button>
         )
-    };
-};
+    }
+}
 
 export default Button
