e93006d48e4ba047a1e1b0ec781d2b895aaa359f
allankildare <allankildare27@gmail.com>
2021-08-16 16:20:05 -0300

    feat: header alterado (menu adicionado)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import style from './Header.module.css'
 
 class Header extends Component {
     render() {
         return (
             <header>
-                Header
+                <nav>
+                    <ul className={style.menu}l>
+                        <li>Drinks</li>
+                        <li>Sobre nós</li>
+                        <li className={style.logo}>Bons Drinks</li>
+                        <li>Nosso time</li>
+                        <li>Contato</li>
+                    </ul>
+                </nav>
             </header>
         )
     }
