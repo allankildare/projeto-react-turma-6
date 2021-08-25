aff9cdf6bd4fd83e437de9f97d9d962bfc29c5f0
allankildare <allankildare27@gmail.com>
2021-08-24 23:04:47 -0300

    chore: import de componentes

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,25 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from '../Button/Button'
+import Contador from '../Contador/Contador'
+import InputState from '../InputState/InputState'
 
 export default function AboutUs() {
-    const [contador, setContador] = useState(0)
-    const [strAtual, setStrAtual] = useState('')
-
-    const atualizaEstado = () => {
-        setContador(contador+1)
-    }
-
-    const lidaComInput = ({ target }) => {
-        setStrAtual(target.value)
-    }
+    
     
     return (
         <div style={{ margin: '0 auto' }}>
-            <h1>Vamos contar!</h1>
-            <h2>{contador}</h2>
-            <button onClick={atualizaEstado}>Atualiza estado!</button>
-            <input onChange={lidaComInput} type="text" />
-            <h3>{strAtual}</h3>
+            <Contador />
+            <InputState />
             <Button texto="Turma 6" />
         </div>
     )
