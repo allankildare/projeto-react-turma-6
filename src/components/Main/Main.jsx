59f9f56b03f7746c8215e0b8092b00b214c36c23
allankildare <allankildare27@gmail.com>
2021-08-24 23:05:25 -0300

    refactor: alteracao para componente funcional

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { MainStyled } from './styles'
 
-class Main extends Component {
-    render() {
-        return (
-            <MainStyled>
-                <h1 className="logo">Bons Drinks</h1>
-            </MainStyled>
-        )
-    }
+function Main(){
+    return (
+        <MainStyled>
+            <h1 className="logo">Bons Drinks</h1>
+        </MainStyled>
+    )
 }
 
 export default Main
