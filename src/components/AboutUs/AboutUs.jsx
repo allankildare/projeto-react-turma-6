05bfcfffa92c24ae3e24cbc8df1fb3b853a887ed
allankildare <allankildare27@gmail.com>
2021-08-25 12:37:39 -0300

    chore: remocao do componente Button

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import Button from '../Button/Button'
 import Contador from '../Contador/Contador'
 import InputState from '../InputState/InputState'
 
 export default function AboutUs() {
-    
-    
     return (
         <div style={{ margin: '0 auto' }}>
             <Contador />
             <InputState />
-            <Button texto="Turma 6" />
         </div>
     )
 }
