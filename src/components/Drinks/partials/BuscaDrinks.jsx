8cce6385b8982f28dc93a031b645cdd0a4e3dae7
allankildare <allankildare27@gmail.com>
2021-08-31 09:55:43 -0300

    feat: componente para busca de drinks

---
+++ src/components/Drinks/partials/BuscaDrinks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BuscaDrinks = () => {
+    return (
+        <div>
+            <h1 className="centeredHeading">Busca de drinks</h1>
+        </div>
+    )
+}
