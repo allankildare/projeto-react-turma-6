91b86e860ae1fc53b29b240e16c12cd2c6202a83
allankildare <allankildare27@gmail.com>
2021-08-31 09:56:33 -0300

    feat: componente para exibicao de drinks populares

---
+++ src/components/Drinks/partials/DrinksPopulares.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DrinksPopulares = () => {
+    return (
+        <div>
+            <h1 className="centeredHeading">Drinks populares</h1>
+        </div>
+    )
+}
