2d0f501a1f582d0f7f7d5b695a60d08a5d7af572
allankildare <allankildare27@gmail.com>
2021-08-31 10:01:00 -0300

    feat: importacao de componentes, provider da context api, especificacao de rotas exatas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Form from './components/Form/Form'
@@ -6,43 +6,49 @@ import Footer from './components/Footer/Footer'
 import Drinks from './components/Drinks/Drinks'
 import Main from './components/Main/Main'
 import OurTeam from './components/OurTeam/OurTeam'
+import AboutUs from './components/AboutUs/AboutUs'
+import SubDrinks from './components/Drinks/SubDrinks'
+import UserProvider from './components/UserProvider/UserProvider'
 import { 
   BrowserRouter as Router,
   Switch,
   Route } from 'react-router-dom'
-import AboutUs from './components/AboutUs/AboutUs'
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className="App">  
         <Router>
           <Header />
-
-          <Switch>
-            <Route path="/" exact>
-              <Main />
-            </Route>
-            <Route path="/drinks" children={<Drinks />} />
-            <Route path="/sobre-nos">
-              <AboutUs />  
-            </Route> 
-            <Route path="/nosso-time">
-              <OurTeam />
-            </Route>
-            <Route path="/contato">
-              <Form />
-            </Route>
-            <Route path="*">
-              <h1>Erro 404</h1>
-            </Route>
-          </Switch>
+          <UserProvider>
+            <Switch>
+              <Route path="/" exact>
+                <Main />
+              </Route>
+              <Route path="/drinks" exact>
+                <Drinks />
+              </Route>
+              <Route path="/drinks/:subpagina">
+                <SubDrinks />
+              </Route>
+              <Route path="/sobre-nos" exact>
+                <AboutUs />  
+              </Route> 
+              <Route path="/nosso-time" exact>
+                <OurTeam />
+              </Route>
+              <Route path="/contato" exact>
+                <Form />
+              </Route>
+              <Route path="*">
+                <h1>Erro 404</h1>
+              </Route>
+            </Switch>
+          </UserProvider>
 
         </Router>
         <Footer />
       </div>
     )
-  }
 }
 
 export default App
