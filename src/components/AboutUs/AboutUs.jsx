61c35d462de6ea280789bca290bf0ae89b80a04f
allankildare <allankildare27@gmail.com>
2021-08-31 09:54:32 -0300

    chore: remocao de componentes e adicao de cabecalho

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Contador from '../Contador/Contador'
-import InputState from '../InputState/InputState'
 
 export default function AboutUs() {
     return (
-        <div style={{ margin: '0 auto' }}>
-            <Contador />
-            <InputState />
+        <div>
+            <h1 className="centeredHeading">
+                Sobre nós
+            </h1>
         </div>
     )
 }
